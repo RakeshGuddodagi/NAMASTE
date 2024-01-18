fd659da9c0fce8f2ed23629b723ad67027b44587
Rockey <rakeshguddodagi5@gmail.com>
2024-01-18 22:04:10 +0530

    add to git

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h1",{},"this is namaste react"),
         React.createElement("h2",{},"I am h2 tag"),
     ]),
     React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h1",{},"this is praveen from webxinfinity"),
         React.createElement("h2",{},"I am h2 tag"),
     ])
 
