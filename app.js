e21853031f16ed6cd735b82a970c2ad585b96dc4
Rockey <rakeshguddodagi5@gmail.com>
2024-01-23 09:20:41 +0530

    episode-01

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
 
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://static.vecteezy.com/system/resources/previews/002/285/943/original/food-service-logo-design-template-free-vector.jpg"/>
+                
+            </div>
+            <div className="nav-items">
+                <ul>
+                  <li>Home</li>
+                  <li>About-Us</li>
+                  <li>Contact-Us</li>
+                  <li>Cart</li>
+                </ul>
+            </div>
 
+        </div>
+    )
+};
  
 const RestaurantCard = (props) => {
     const {resData} = props;
